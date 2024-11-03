96af53adbd14b2f3c918c19bb2131370ea107540
JakubPoznański <jqbpoz02@gmail.com>
2024-11-03 14:37:37 +0100

    add built files

---
+++ Static Website/build/index.450d92aa.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".header__menu-button"),t=document.querySelector(".header__menu");e.addEventListener("click",()=>{t.classList.toggle("open")});
+//# sourceMappingURL=index.450d92aa.js.map
